8925a11a542d091bc77efdf56e54cc12bf361333
secbro <xinyoulingxi2008@126.com>
2019-04-02 09:02:23 +0800

    add hello-world

---
+++ hello-world/src/main/java/com/secbro2/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.secbro2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
