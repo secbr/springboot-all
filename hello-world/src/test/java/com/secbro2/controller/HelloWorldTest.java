e8ddb5f914e35f3bb18c1cb79f0366ebbbc3d027
secbro <xinyoulingxi2008@126.com>
2019-04-03 11:18:42 +0800

    mockmvc

--- hello-world/src/test/java/com/secbro2/controller/HelloWorldTest.java
+++ hello-world/src/test/java/com/secbro2/controller/HelloWorldTest.java
@@ -3,11 +3,14 @@ package com.secbro2.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -22,15 +25,20 @@ public class HelloWorldTest {
 
 	private MockMvc mockMvc;
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
 	@Before
 	public void setup() {
-		mockMvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+//		mockMvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
 
 	@Test
 	public void testHello() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("hello world!")));
+				.andExpect(content().string(equalTo("hello world!")))
+				.andDo(MockMvcResultHandlers.print());
 	}
 }
