744649565c5975bf76e676fbab146a9f56cadfd5
secbro <xinyoulingxi2008@126.com>
2019-04-07 10:33:36 +0800

    init banner

---
+++ springboot-banner/src/main/java/com/secbro2/banner/SpringbootBannerApplication.java
@@ -0,0 +1,13 @@
+package com.secbro2.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBannerApplication.class, args);
+	}
+
+}
