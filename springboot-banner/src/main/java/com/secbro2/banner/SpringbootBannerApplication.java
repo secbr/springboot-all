240544233d6efbb5da8be48e663cc1eba102a7c7
secbro <xinyoulingxi2008@126.com>
2019-04-07 15:56:02 +0800

    init banner

--- springboot-banner/src/main/java/com/secbro2/banner/SpringbootBannerApplication.java
+++ springboot-banner/src/main/java/com/secbro2/banner/SpringbootBannerApplication.java
@@ -1,8 +1,10 @@
 package com.secbro2.banner;
 
 import org.springframework.boot.Banner;
+import org.springframework.boot.ImageBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * banner学习
@@ -15,7 +17,11 @@ public class SpringbootBannerApplication {
 	public static void main(String[] args) {
 
 		SpringApplication app = new SpringApplication(SpringbootBannerApplication.class);
-		app.setBannerMode(Banner.Mode.CONSOLE);
+
+//		Banner banner = new ImageBanner(new ClassPathResource("banner1.png"));
+//		app.setBanner(banner);
+//		app.setBannerMode(Banner.Mode.OFF);
+
 		app.run(args);
 	}
 
