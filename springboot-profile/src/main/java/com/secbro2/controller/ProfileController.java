18ecc4095f8668c58723909b3881bf7daafc709a
secbro <xinyoulingxi2008@126.com>
2019-12-23 10:59:41 +0800

    add profile

--- springboot-profile/src/main/java/com/secbro2/controller/ProfileController.java
+++ springboot-profile/src/main/java/com/secbro2/controller/ProfileController.java
@@ -1,5 +1,7 @@
-package com.secbro2;
+package com.secbro2.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,5 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProfileController {
 
+	@Value("${user.username}")
+	private String username;
 
+	@Value("${user.phone}")
+	private String phone;
+
+	@GetMapping
+	public String getInfo() {
+
+		return "username=" + username + ";phone=" + phone;
+	}
 }
