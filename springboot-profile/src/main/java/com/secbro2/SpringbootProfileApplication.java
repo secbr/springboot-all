a6f2c66bd3c9f0276c3061c83355a6a323512860
secbro <xinyoulingxi2008@126.com>
2019-12-23 10:59:09 +0800

    add profile

---
+++ springboot-profile/src/main/java/com/secbro2/SpringbootProfileApplication.java
@@ -0,0 +1,13 @@
+package com.secbro2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProfileApplication.class, args);
+	}
+
+}
---
+++ springboot-profile/src/main/java/com/secbro2/controller/ProfileController.java
@@ -0,0 +1,14 @@
+package com.secbro2;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sec
+ * @version 1.0
+ * @date 2019/12/23 10:28 AM
+ **/
+@RestController
+public class ProfileController {
+
+
+}
