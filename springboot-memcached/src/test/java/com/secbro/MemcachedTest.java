45988b87b87d40b5160cc43d2e030d2235303109
secbro <xinyoulingxi2008@126.com>
2020-03-26 12:09:44 +0800

    memcached

--- springboot-memcached/src/test/java/com/secbro/MemcachedTest.java
+++ springboot-memcached/src/test/java/com/secbro/MemcachedTest.java
@@ -2,9 +2,12 @@ package com.secbro;
 
 import lombok.extern.slf4j.Slf4j;
 import net.rubyeye.xmemcached.MemcachedClient;
+import net.rubyeye.xmemcached.exception.MemcachedException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author sec
@@ -29,6 +32,44 @@ public class MemcachedTest {
 
 		// 删除缓存
 		memcachedClient.delete("wechat");
+	}
+
+	@Test
+	public void testAdd() throws InterruptedException, MemcachedException, TimeoutException {
+		// 先增加
+		memcachedClient.set("wechat", 1, "公众号：程序新视界");
+
+		Thread.sleep(2000);
+
+		if (!memcachedClient.add("wechat", 0, "二师兄")) {
+			System.out.println("(1)新增失败");
+		}
+
+		System.out.println((String) memcachedClient.get("wechat"));
+//		memcachedClient.delete("wechat");
+	}
+
+	@Test
+	public void testReplace() throws InterruptedException, MemcachedException, TimeoutException {
+		memcachedClient.set("hello", 0, "Hello Memcached!");
+		if(!memcachedClient.replace("hello", 0, "Hello Memcached!")){
+			System.out.println("替换失败");
+		}
+
+		System.out.println((String) memcachedClient.get("hello"));
+	}
+
+	@Test
+	public void testAppend() throws InterruptedException, MemcachedException, TimeoutException {
+		memcachedClient.delete("hello");
+
+		memcachedClient.set("hello", 0, "Memcached");
+
+		memcachedClient.prepend("hello","Hello ");
+
+		memcachedClient.append("hello", "!");
+
+		System.out.println((String) memcachedClient.get("hello"));
 	}
 
 
