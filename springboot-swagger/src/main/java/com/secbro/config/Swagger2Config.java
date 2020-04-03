e1ba5f9cf8d9164d8ece50c17be0dcd5cd5ce6d5
secbro <xinyoulingxi2008@126.com>
2020-04-03 11:18:24 +0800

    on or off

--- springboot-swagger/src/main/java/com/secbro/config/Swagger2Config.java
+++ springboot-swagger/src/main/java/com/secbro/config/Swagger2Config.java
@@ -1,7 +1,9 @@
 package com.secbro.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -18,11 +20,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  **/
 @Configuration
 @EnableSwagger2
+@Profile({"dev","test"})
 public class Swagger2Config {
+
+	@Value("${swagger.isShow}")
+	private boolean isShow;
 
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.enable(isShow)
 				.apiInfo(apiInfo())
 				// 返回ApiSelectorBuilder实例，用来控制对哪些接口进行展现
 				.select()
