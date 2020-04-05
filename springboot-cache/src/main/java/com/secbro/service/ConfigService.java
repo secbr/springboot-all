4c789067ef26570149983973102ee1178fbc651b
secbro <xinyoulingxi2008@126.com>
2020-04-05 11:06:02 +0800

    spring cache

--- springboot-cache/src/main/java/com/secbro/service/ConfigService.java
+++ springboot-cache/src/main/java/com/secbro/service/ConfigService.java
@@ -2,6 +2,8 @@ package com.secbro.service;
 
 import com.secbro.entity.SystemConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class ConfigService {
 
+	public static int count = 0;
+
 	@Cacheable(value = "systemConfig")
 	public SystemConfig getConfig() {
 		log.info("获取配置信息...");
@@ -22,5 +26,38 @@ public class ConfigService {
 		config.setValue("0");
 		return config;
 	}
+
+	@Cacheable(value = "systemConfig-2", key = "#p0")
+	public SystemConfig getConfigByIdAndKey(int id, String key) {
+		log.info("getConfigById获取配置信息...id={},key={}", id, key);
+		return new SystemConfig();
+	}
+
+	@Cacheable(value = "systemConfig-1", condition = "#id >= 10")
+	public SystemConfig getConfigById(int id) {
+		log.info("getConfigById获取配置信息...id={}", id);
+		return new SystemConfig();
+	}
+
+
+	@Cacheable(value = "getInt", key = "#id")
+	public int getInt(int id) {
+		log.info("getInt获取配置信息...id={}", id);
+		return count;
+	}
+
+	/**
+	 * 静态变量递增
+	 */
+	@CachePut(value = "getInt", key = "#id")
+	public int addInt(int id) {
+		return ++count;
+	}
+
+	@CacheEvict(value = "getInt", allEntries = true, beforeInvocation = true)
+	public void clearInt() {
+		// 操作数据库等删除存储数据
+	}
+
 
 }
--- springboot-cache/src/test/java/com/secbro/SpringCacheTest.java
+++ springboot-cache/src/test/java/com/secbro/SpringCacheTest.java
@@ -27,4 +27,45 @@ public class SpringCacheTest {
 		log.info("---第2次获取完毕");
 	}
 
+	@Test
+	public void testCacheable1() {
+		configService.getConfigByIdAndKey(1, "config1");
+		log.info("---第1次获取完毕");
+		configService.getConfigByIdAndKey(1, "config2");
+		log.info("---第2次获取完毕");
+	}
+
+	@Test
+	public void testCacheable2() {
+		configService.getConfigById(11);
+		log.info("---第1次获取完毕");
+		configService.getConfigById(11);
+		log.info("---第2次获取完毕");
+	}
+
+	@Test
+	public void testCacheable3() {
+		int count = configService.getInt(1);
+		log.info("---第1次获取完毕int={}",count);
+		count = configService.getInt(1);
+		log.info("---第2次获取完毕int={}",count);
+		configService.addInt(1);
+
+		count = configService.getInt(1);
+		log.info("---第3次获取完毕int={}",count);
+
+	}
+
+	@Test
+	public void testCacheable4() {
+		int count = configService.getInt(1);
+		log.info("---第1次获取完毕int={}",count);
+		count = configService.getInt(1);
+		log.info("---第2次获取完毕int={}",count);
+		configService.clearInt();
+
+		count = configService.getInt(1);
+		log.info("---第3次获取完毕int={}",count);
+
+	}
 }
